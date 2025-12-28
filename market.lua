531b4ca5c676f58d269b9eeac3f7fa2dad3d490d
Justin Nelson <smaugoferebor@gmail.com>
2025-12-28 00:09:29 -0500

    So far, I've been entirely unable to find a way to get any arbitrary text from a client in a lua file with the standard API exposed to the lua. This is probably good for security, but bad for my strictly no client-side file solution for this little market idea. I've resorted to requiring specific quantities of specific items in the backpack slot to incidate to the server that the player wants to buy, sell, or query their balance. It's the best solution I've come up with so far now that I have given up on retrieving arbitrary text from the client.

--- market.lua
+++ market.lua
@@ -1,3 +1,5 @@
+print("[PZ Market] Loading the Project Zomboid Market!")
+
 -- Returns the currency balance for the specified player.
 -- Ensures that the player has currency data.
 function GetBalance(player)
@@ -18,91 +20,111 @@ function ModifyBalance(player, amount)
 end
 
 -- Attempts to sell the specified item from the specified player's inventory.
-function SellItem(player, itemType)
-    local inventory = player:getInventory()
-
-    -- Ensure that the player has this item to sell.
-    local item = inventory:FindAndReturn(itemType)
-    if not item then
-        player:sendChatMessage("You don't have that item.")
-        return
-    end
-
+function SellItem(player, item)
     -- Ensure that there is a price definition for this item.
-    local price = ItemPrices[itemType] or 0
+    local price = ItemPrices[item:getType()] or 0
     if price <= 0 then
-        player:sendChatMessage("That item cannot be sold.")
+        player:sendChatMessage(item:getType() .. " cannot be sold.")
         return
     end
 
     -- Remove the item from the player's inventory and imburse the player accordingly.
-    inventory:Remove(item)
+    item:getContainer():Remove(item)
     ModifyBalance(player, price)
-    player:sendChatMessage("Sold " .. itemType .. " for " .. price)
+    player:sendChatMessage("Sold " .. item:getType() .. " for " .. price)
 end
 
 -- Attempts to purchase the specified item for the specified player.
-function BuyItem(player, itemType)
+function BuyItem(player, item)
     -- Ensure that there is a price definition for this item.
-    local price = ItemPrices[itemType] or 0
+    local price = ItemPrices[item:getType()] or 0
     if price <= 0 then
-        player:sendChatMessage("That item cannot be purchased.")
+        player:sendChatMessage(item:getType() .. " cannot be purchased.")
         return
     end
 
     -- Ensure that the player actually has the required balance to make the purchase.
     local balance = GetBalance(player)
     if balance < price then
-        player:sendChatMessage("You don't have enough money to purchase that item. Price: " .. price)
+        player:sendChatMessage("You don't have enough money to purchase " .. item:getType() .. ". Price: " .. price)
         return
     end
 
     -- Attempt to add the item to the player's inventory.
     local inventory = player:getInventory()
-    local purchasedItem = inventory:AddItem(itemType)
+    local purchasedItem = inventory:AddItem(item:getType())
     if not purchasedItem then
-        player:sendChatMessage("Failed to add item to your inventory. You will not be charged.")
+        player:sendChatMessage("Failed to add " .. item:getType() .. " to your inventory. You will not be charged.")
         return
     end
 
     -- Take the corresponding amount of currency from the player's balance.
     ModifyBalance(player, -price)
-    player:sendChatMessage("Purchased " .. itemType .. " for " .. price)
+    player:sendChatMessage("Purchased " .. item:getType() .. " for " .. price)
 end
 
-Events.OnPlayerChat.Add(function(player, message)
-    local args = {}
-
-    for word in message:gmatch("%S+") do
-        table.insert(args, word)
+Events.EveryTenMinutes.Add(function()
+    local players = getOnlinePlayers()
+    if not players then
+        return 
     end
 
-    local cmd = args[1]
+    print("[PZ Market] Scanning player inventories.")
 
-    -- The player wants to query their balance.
-    if cmd == "/balance" then
-        local balance = GetBalance(player)
-        player:sendChatMessage("Balance: " .. balance)
-        return false
-    end
+    -- Iterate over every online player
+    for i = 0, players:size() - 1 do
+        local player = players:get(i)
 
-    -- The player wants to buy an item.
-    if cmd == "/buy" then
-        if not args[2] then
-            player:sendChatMessage("You must specify the item type to purchase.")
-            return false
+        local backpack = player:getClothingItem_Back()
+        if backpack then
+            local inv = backpack:getInventory()
+            local items = inv:getItems()
+
+            if items:size() == 2 then
+                -- Check if the player wants to query their balance
+                local totalRag = 0
+                local totalRagDirty = 0
+                for j = 0, 1 do
+                    local item = items:get(j)
+                    if item:getName():lower() == "rag" then
+                        totalRag = item:getCount()
+                    elseif item:getName():lower() == "rag (dirty)" then
+                        totalRagDirty = item:getCount()
+                    end
+                end
+
+                -- The player wants to query their balance
+                if totalRag == 20 and totalRagDirty == 20 then
+                    local balance = GetBalance(player)
+                    print("[PZ Market] " .. player:getDisplayName() .. " Balance: " .. balance)
+                    player:sendChatMessage("Balance: " .. tostring(balance)) 
+                end
+
+            elseif items:size() == 3 then
+                -- Check if the player wants to sell or purchase an item
+                local totalRag = 0
+                local totalRagDirty = 0
+                local thirdItem
+                for j = 0, 2 do
+                    local item = items:get(j)
+                    if item:getName():lower() == "rag" then
+                        totalRag = item:getCount()
+                    elseif item:getName():lower() == "rag (dirty)" then
+                        totalRagDirty = item:getCount()
+                    else
+                        thirdItem = item
+                    end
+                end
+
+                if totalRag == 20 and totalRagDirty == 21 then
+                    -- The player wants to sell an item
+                    SellItem(player, thirdItem)
+
+                elseif totalRag == 21 and totalRagDirty == 20 then
+                    -- The player wants to buy an item
+                    BuyItem(player, thirdItem)
+                end
+            end
         end
-        BuyItem(player, args[2])
-        return false
-    end
-
-    -- The player wants to sell an item.
-    if cmd == "/sell"then
-        if not args[2] then
-            player:sendChatMessage("You must specify the item type to sell.")
-            return false
-        end
-        SellItem(player, args[2])
-        return false
     end
 end)
