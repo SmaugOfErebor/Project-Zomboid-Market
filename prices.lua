2b813c637835c5b3b3ded81d85b6e09fd030d10d
Justin Nelson <smaugoferebor@gmail.com>
2025-12-29 16:43:37 -0500

    Added more items to the price list.

--- prices.lua
+++ prices.lua
@@ -1,21 +1,39 @@
 ItemPrices = {
+    ["Base.BathTowel"] = 1,
     ["Base.Bowl"] = 1,
+    ["Base.Branch_Broken"] = 6,
     ["Base.BroccoliSeed"] = 1,
     ["Base.ChamomileSeed"] = 1,
+    ["Base.Dogbane"] = 1,
+    ["Base.Doily"] = 1,
+    ["Base.ElectronicsScrap"] = 5,
     ["Base.GolfBall"] = 1,
     ["Base.GolfTee"] = 1,
     ["Base.GrassTuft"] = 1,
+    ["Base.IndexCard"] = 1,
     ["Base.LeekSeed"] = 1,
+    ["Base.LetterHandwritten"] = 1,
+    ["Base.Money"] = 1,
+    ["Base.Paperwork"] = 1,
     ["Base.Plasticbag"] = 1,
     ["Base.PotatoSeed"] = 1,
+    ["Base.Receipt"] = 1,
+    ["Base.Receiver"] = 5,
     ["Base.RedRadishSeed"] = 1,
     ["Base.RippedSheets"] = 1,
     ["Base.RippedSheetsDirty"] = 1,
+    ["Base.Sheet"] = 5,
+    ["Base.SheetPaper2"] = 1,
+    ["Base.Socks_Ankle"] = 1,
     ["Base.ThymeSeed"] = 1,
+    ["Base.Tissue"] = 1,
     ["Base.TomatoSeed"] = 1,
-    ["Base.UnusableWood"] = 1,
+    ["Base.Twigs"] = 1,
+    ["Base.UnusableWood"] = 4,
     ["Base.Wallet_Male"] = 1,
     ["Base.WildGarlicSeed"] = 1
 }
+
+-- Items that can be used as fuel in a fireplace are priced as fuel_minutes rounded up to the nearest 10, divided by 10.
 
 return true
