342e7a6a3276725c12993fe8a1b20603464900a6
Justin Nelson <smaugoferebor@gmail.com>
2025-12-29 14:50:05 -0500

    Adding a few more items to the price sheet.

--- prices.lua
+++ prices.lua
@@ -1,7 +1,21 @@
 ItemPrices = {
+    ["Base.Bowl"] = 1,
+    ["Base.BroccoliSeed"] = 1,
+    ["Base.ChamomileSeed"] = 1,
+    ["Base.GolfBall"] = 1,
+    ["Base.GolfTee"] = 1,
     ["Base.GrassTuft"] = 1,
+    ["Base.LeekSeed"] = 1,
+    ["Base.Plasticbag"] = 1,
+    ["Base.PotatoSeed"] = 1,
+    ["Base.RedRadishSeed"] = 1,
     ["Base.RippedSheets"] = 1,
-    ["Base.RippedSheetsDirty"] = 1
+    ["Base.RippedSheetsDirty"] = 1,
+    ["Base.ThymeSeed"] = 1,
+    ["Base.TomatoSeed"] = 1,
+    ["Base.UnusableWood"] = 1,
+    ["Base.Wallet_Male"] = 1,
+    ["Base.WildGarlicSeed"] = 1
 }
 
 return true
